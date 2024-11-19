75df772b4b10132b00a43d96b4c6f9b4b37181a7
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-19 11:57:45 -0600

    Create ResultsScreen.js

---
+++ src/Components/ResultsScreen.js
@@ -0,0 +1,22 @@
+function ResultsScreen({ points, maxPoints, highscore }) {
+  const percentage = (points / maxPoints) * 100;
+  const highestPercent = Math.ceil((highscore / maxPoints) * 100);
+
+  let emoji;
+  if (percentage > 89) emoji = "🏆";
+  if (percentage <= 89 && percentage >= 80) emoji = "⭐";
+  if (percentage <= 79 && percentage >= 70) emoji = "😕";
+  if (percentage < 69) emoji = "👎";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> Your score is{" "}
+        <strong>{Math.ceil(percentage)}%</strong> ({points} out of {maxPoints}{" "}
+        points){" "}
+      </p>
+      <p className="highscore">(Your highest score: {highestPercent}%)</p>
+    </>
+  );
+}
+
+export default ResultsScreen;
